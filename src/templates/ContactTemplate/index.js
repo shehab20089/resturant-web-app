2a20f7f28f8155c0e146849a7a11625497fe648a
shehab20089 <shehabmohsen1995@gmail.com>
2023-01-03 09:33:52 +0200

    minor fix

--- src/templates/ContactTemplate/index.js
+++ src/templates/ContactTemplate/index.js
@@ -11,7 +11,7 @@ export const contactTemplate = /*html*/ `
     <path fill="#db5461" d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z" />
 </svg>90 Faisel Street,Giza,Egypt</p>
  <div class="location-img">
- <img  src=${locationImage}/>
+ <img  src=${locationImage}>
  </div>
  </div>
  </div>
